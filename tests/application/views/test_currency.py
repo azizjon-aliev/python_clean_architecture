1bfc11811d5365174eb23dbe224e455349fb5743
Azizjon <azizaliev2337@gmail.com>
2023-10-18 11:37:12 +0500

    change

--- tests/application/views/test_currency.py
+++ tests/application/views/test_currency.py
@@ -1,4 +1,5 @@
 import pytest
+
 from django.urls import reverse
 from rest_framework.test import APIClient
 
@@ -6,8 +7,8 @@ from rest_framework.test import APIClient
 @pytest.mark.django_db
 def test_list_currency_api_view():
     client = APIClient()
-    url = reverse('currency-list')
+    url = reverse("currency-list")
 
-    response = client.get(url, format='json')
+    response = client.get(url, format="json")
 
     print(response)

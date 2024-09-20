af09700c5d6f8c7f3e0a46d6999b15948cf0ff7f
Azimjon <azimausupov@gmail.com>
2024-09-20 18:34:23 +0300

    Merge branch 'upstream/develop' into develop

--- src/infrastructure/models/account.py
+++ src/infrastructure/models/account.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
-from src.infrastructure.managers import UserManager
 from src.infrastructure.models.base import Auditable, Timestampble
 
 
 class User(AbstractUser, Timestampble, Auditable):
-    objects = UserManager()
+    pass

75f579734781f2b565bd0617497577e5474ef9b0
Aziz <azizaliev2337@gmail.com>
2024-11-14 12:30:06 +0500

    refactor: use typing python 3.12

--- src/infrastructure/models/account.py
+++ src/infrastructure/models/account.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from src.infrastructure.constaints import Role
 from src.infrastructure.managers import UserManager
@@ -7,35 +8,76 @@ from src.infrastructure.models.base import AuditableMixin, TimestampMixin
 
 
 class User(AbstractUser, TimestampMixin, AuditableMixin):
+    """
+    Custom User model extending Django's AbstractUser
+    with additional fields and functionality.
+
+    This model replaces the default username with a phone number for authentication.
+    It includes roles, email, phone verification, and OTP management.
+    """
+
+    # Removing default fields from AbstractUser
     username = None
     first_name = None
     last_name = None
 
-    base_role = Role.STAFF
-    role = models.CharField(verbose_name="Роль", max_length=50, choices=Role.choices)
+    # Constants
+    BASE_ROLE = Role.STAFF
+
+    # Fields
+    role = models.CharField(
+        _("Role"),
+        max_length=50,
+        choices=Role.choices,
+        default=BASE_ROLE,
+        help_text=_("User role within the application."),
+    )
     email = models.EmailField(
-        verbose_name="Email",
+        _("Email"),
         blank=True,
         unique=True,
         null=True,
         max_length=200,
+        help_text=_("Unique email address for the user."),
     )
     phone = models.CharField(
+        _("Phone"),
         max_length=15,
         unique=True,
-        verbose_name="Телефон",
-        blank=False,
-        help_text="Введите с 992 и 9-значный номер телефона",
+        help_text=_(
+            "Enter a phone number starting with 992 followed by a 9-digit number."
+        ),
     )
-    otp = models.IntegerField(default=0)
-    is_verified = models.BooleanField(default=False)
+    otp = models.PositiveIntegerField(
+        _("OTP"),
+        default=0,
+        help_text=_("One-Time Password for user verification."),
+    )
+    is_verified = models.BooleanField(
+        _("Is Verified"),
+        default=False,
+        help_text=_("Indicates whether the user's phone number has been verified."),
+    )
 
+    # Authentication settings
     USERNAME_FIELD = "phone"
     REQUIRED_FIELDS = []
 
+    # Managers
     objects = UserManager()
 
-    def save(self, *args, **kwargs):
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return f"{self.phone} - {self.email or 'No Email'}"
+
+    def save(self, *args, **kwargs) -> None:
+        """
+        Override the save method to set the default role upon creation.
+        """
         if not self.pk:
-            self.role = self.base_role
-        return super().save(*args, **kwargs)
+            self.role = self.BASE_ROLE
+        super().save(*args, **kwargs)
